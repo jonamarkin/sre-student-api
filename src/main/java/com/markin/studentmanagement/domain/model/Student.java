19f68404f4b585d55bb05aa33f317543283bc76d
Jonathan Ato Markin <jona10711@gmail.com>
2024-03-09 15:30:34 +0100

    Student model created in the domain

--- src/main/java/com/markin/studentmanagement/domain/model/Student.java
+++ src/main/java/com/markin/studentmanagement/domain/model/Student.java
@@ -1,6 +1,19 @@
 package com.markin.studentmanagement.domain.model;
 
-public class Student {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+    private UUID id;
+    private String firstName;
+    private String lastName;
 
 }

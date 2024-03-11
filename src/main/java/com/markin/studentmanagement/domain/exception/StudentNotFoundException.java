86f96ca470bc427703955307821a55c7a0d2a69d
Jonathan Ato Markin <jona10711@gmail.com>
2024-03-11 17:48:59 +0100

    Architecture overhaul

--- src/main/java/com/markin/studentmanagement/domain/exception/StudentNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.markin.studentmanagement.domain.exception;
-
-public class StudentNotFoundException extends RuntimeException{
-
-    public StudentNotFoundException(String message){
-        super(message);
-    }
-}

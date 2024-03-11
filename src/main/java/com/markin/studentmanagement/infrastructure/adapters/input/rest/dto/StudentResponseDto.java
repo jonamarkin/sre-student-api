61a2aa58577c2f3b74828297b1a5917e4cd0e8ca
Jonathan Ato Markin <jona10711@gmail.com>
2024-03-11 19:44:26 +0100

    Student response Dto added

--- src/main/java/com/markin/studentmanagement/infrastructure/adapters/input/rest/dto/StudentResponseDto.java
+++ src/main/java/com/markin/studentmanagement/infrastructure/adapters/input/rest/dto/StudentResponseDto.java
@@ -1,4 +1,19 @@
 package com.markin.studentmanagement.infrastructure.adapters.input.rest.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class StudentResponseDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDateTime dateOfBirth;
 }

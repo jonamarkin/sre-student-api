8896cf34407d8d9b083338cf77cffd6cc75426fa
Jonathan Ato Markin <jona10711@gmail.com>
2024-03-15 00:23:11 +0100

    Test add student

--- src/test/java/com/markin/studentmanagement/StudentServiceTest.java
+++ src/test/java/com/markin/studentmanagement/StudentServiceTest.java
@@ -1,12 +1,15 @@
 package com.markin.studentmanagement;
 
 import com.markin.studentmanagement.application.port.output.StudentOutputPort;
+import com.markin.studentmanagement.domain.model.Student;
 import com.markin.studentmanagement.domain.service.StudentService;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class StudentServiceTest {
@@ -20,6 +23,10 @@ public class StudentServiceTest {
         studentService = new StudentService(studentOutputPort);
     }
 
-//    @Test
-//    public void testAddStudent()
+    @Test
+    public void testAddStudent(){
+        Student student = new Student();
+        when(studentOutputPort.save(student)).thenReturn(student);
+        assertEquals(student, studentService.addStudent(student));
+    }
 }
